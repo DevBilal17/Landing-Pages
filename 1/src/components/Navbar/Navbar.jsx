f5ae9060473b6550d94cb32083b5f2266cfa6147
DevBilal17 <muhammadbilalmb03122@gmail.com>
2026-08-09 15:10:09 +0500

    Navbar content done

---
+++ 1/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import profileImg from "../../assets/me.jpeg"
+const Navbar = () => {
+  return (
+    <nav className='container mx-auto'>
+      <div>
+        <h2>2rism</h2>
+        <img src="/favicon.svg" alt="" />
+      </div>
+    
+        <ul>
+            <li>home</li>
+            <li>hotels</li>
+            <li>restaurants</li>
+            <li>tours</li>
+            <li>destinations</li>
+            <li>activities</li>
+            <li>contact</li>
+        </ul>
+      
+      <div>
+        <div>
+            <p>Hola</p>
+            <h4>M. Bilal</h4>
+        </div>
+        <div>
+            <img src={profileImg} alt="" />
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
