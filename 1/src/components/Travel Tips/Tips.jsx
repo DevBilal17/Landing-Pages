ca6e9b348213c8be9466f04fdd2ba6d82de99126
DevBilal17 <muhammadbilalmb03122@gmail.com>
2026-08-10 10:30:22 +0500

    Advice section for Desktop Done

--- 1/src/components/Travel Tips/Tips.jsx
+++ 1/src/components/Travel Tips/Tips.jsx
@@ -1,48 +1,54 @@
-import React from 'react'
-import { TIPS } from '../../data/tips'
+import React from "react";
+import { TIPS } from "../../data/tips";
 import { RiCalendar2Line } from "react-icons/ri";
 import { RiUserLine } from "react-icons/ri";
-import { FaRegComment } from "react-icons/fa";
+import { FaArrowRight, FaRegComment } from "react-icons/fa";
 const Tips = () => {
   return (
-    <div  className='container mx-auto px-3 py-10'>
-      <div className='flex justify-between'>
-        <h2 className='text-2xl font-bold text-[#161414]'>Travel Tips and Advice</h2>
-        <button>View all</button>
+    <div className="container mx-auto px-3 py-10">
+      <div className="flex justify-between">
+        <h2 className="text-2xl font-bold text-[#161414]">
+          Travel Tips and Advice
+        </h2>
+        <button className="p-2.5 flex items-center border border-[#7B61FF] rounded-xl text-[#7B61FF] gap-1 bg-[#F6F4FF] cursor-pointer">View all <FaArrowRight/></button>
       </div>
 
-      <div className='mt-12 grid grid-cols-2 gap-8'>
-        {
-            TIPS.map((item,index)=>(
-                <div key={item.id}>
-                    <div>
-                     <img src={item.image} alt={item.title} />
-                    </div>
-                    <div>
-                        <h3>{item.title}</h3>
-                        <p>{item.desc}</p>
+      <div className="mt-12 grid grid-cols-2 gap-8">
+        {TIPS.map((item, index) => (
+          <div key={item.id} className="flex gap-8">
+            <div className="h-95 w-79 shrink-0 rounded-3xl overflow-hidden">
+              <img
+                src={item.image}
+                alt={item.title}
+                className="h-full w-full object-cover object-center"
+              />
+            </div>
+            <div className="flex flex-col justify-between">
+            <div>
+                  <h3 className="text-xl text-[#161414] font-bold">{item.title}</h3>
+              <p className="mt-4 text-[#979797]">{item.desc}</p>
 
-                        <div>
-                            <div>
-                                <RiCalendar2Line/>
-                                {item.date}
-                            </div>
-                            <div>
-                                <RiUserLine/>
-                                {item.author}
-                            </div>
-                            <div>
-                                <FaRegComment/>
-                                {item.comments}
-                            </div>
-                        </div>
-                    </div>
+            </div>
+              <div className="flex gap-6">
+                <div className="flex items-center gap-1 text-sm text-[#979797]">
+                  <RiCalendar2Line />
+                  {item.date}
                 </div>
-            ))
-        }
+                <div className="flex items-center gap-1 text-sm text-[#979797]">
+                  <RiUserLine />
+                  {item.author}
+                </div>
+                <div className="flex items-center gap-1 text-sm text-[#979797]">
+                  <FaRegComment />
+                  {item.comments}
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Tips
+export default Tips;
