08b091afe0d3bfbb0985b9768cea0b28c1e892d9
DevBilal17 <muhammadbilalmb03122@gmail.com>
2026-08-10 10:39:52 +0500

    Hotels responsive done

--- 1/src/components/Hotels/Hotels.jsx
+++ 1/src/components/Hotels/Hotels.jsx
@@ -7,11 +7,11 @@ const Hotels = () => {
   return (
     <div className='container mx-auto px-3 py-10'>
       <h2 className='text-2xl font-bold text-[#161414]'>Hotels and Restaurants</h2>
-      <div className='mt-12 grid grid-cols-4 gap-8'>
+      <div className='mt-12 grid lg:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-8'>
         {
             HOTELS.map((item,index)=>(
                 <div key={item.id}>
-                    <div className='max-h-95 h-full rounded-3xl overflow-hidden'>
+                    <div className='sm:h-95 h-60  rounded-3xl overflow-hidden'>
                         <img src={item.image} alt={item.name} className='h-full w-full object-cover object-center' />                     
                     </div>
                     <div className='mt-3'>
