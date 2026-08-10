dffff0dade9aa6d78a192c18f927351c78305223
DevBilal17 <muhammadbilalmb03122@gmail.com>
2026-08-10 10:44:44 +0500

    Activitiess section responsive done

--- 1/src/components/Activities/Activities.jsx
+++ 1/src/components/Activities/Activities.jsx
@@ -9,10 +9,10 @@ const Activities = () => {
         <h2 className='text-2xl font-bold text-[#161414]'>Activites</h2>
         <button className="p-2.5 flex items-center border border-[#7B61FF] rounded-xl text-[#7B61FF] gap-1 bg-[#F6F4FF] cursor-pointer">View all <FaArrowRight/></button>
       </div>
-      <div className='mt-12 grid grid-cols-4 gap-8'>
+      <div className='mt-12 grid lg:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-8'>
         {ACTIVITIES.map((item, index) => (
           <div key={item.id}>
-            <div className='max-h-95 h-full rounded-3xl overflow-hidden'>
+            <div className='sm:h-95 h-60 rounded-3xl overflow-hidden'>
               <img src={item.image} alt={item.name}  className='h-full w-full object-cover object-center'/>
             </div>
             <h3 className="mt-3 text-lg text-[#161414] font-medium">{item.name}</h3>
